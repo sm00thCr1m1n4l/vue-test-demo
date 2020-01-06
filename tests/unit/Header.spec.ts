7acf9589d15ae98d9ff09eb3223ef4b7f433c922
温文海 <wenwenhai@onemt.com.cn>
2020-01-06 15:27:54 +0800

    test: 💍 测试代码调整

--- tests/unit/Header.spec.ts
+++ tests/unit/Header.spec.ts
@@ -2,8 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import Header from '@/components/Header.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+  it('默认slot', () => {
     const wrapper = shallowMount(Header, {
       slots: {
         default: 'header slot'
